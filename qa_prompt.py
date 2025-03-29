1c427395c93a26f9b2eecb8d87fbc3b05cc22953
deghislain@gmail.com <deghislain@gmail.com>
2025-03-29 12:48:46 -0300

    Added a prompt and question answer generation

---
+++ qa_prompt.py
@@ -0,0 +1,41 @@
+example_response = """
+{{
+            "questions":{
+                            { 
+                                "question": "What are large language models?",
+                                "answers": {
+                                        "A": "Large language models are advanced artificial intelligence systems that take some input and generate humanlike text as a response."
+                                        "B": "Large language models are AI protocols."
+                                        "C": "Large language models are a type of neural networks."
+                                },
+                               "solution": "A"
+                            },
+                            { 
+                                "question": "How does Large language models work?",
+                                "answers": {
+                                        "A": "They work by generating casual text"
+                                        "B":  "They work by first analyzing vast amounts of data and creating an internal structure that models"
+                                                the natural language data sets that they’re trained on."
+                                        "C":  "Large language models just use statistic an internet to generate text"
+                                },
+                               "solution": "B"
+                            }
+
+        }}  
+"""
+
+
+def get_qa_system_prompt():
+    return f""" 
+            You are a Subject Mater Expert in multiple domain knowledge. Given a text document and not prior knowledge.
+            Your task is to generate a provided number of questions with answers.
+            The instructions for this task are as follow:
+            All generated questions must come from the provided text document content.
+            You will return a response in json format. Each question must have 3 answers in the multiple choice question style.
+            Only one response shall be correct. The correct answer shall be labeled as solution.
+
+            Here are some examples of questions with their respective answers and solutions in json schema:
+
+            {example_response}
+
+    """
--- quizzer.py
+++ quizzer.py
@@ -1,22 +1,47 @@
 import streamlit as st
 from document_helper import PDFVectorDBLoader
 from langchain_openai import ChatOpenAI
+from qa_prompt import get_qa_system_prompt
+from langchain.schema import SystemMessage, HumanMessage
 import logging
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+llm = ChatOpenAI(model="granite3-dense:latest", temperature=0, base_url="http://localhost:11434/v1", api_key="ollama")
 
 
-def main():
-    label = "Upload multiple PDF document here"
+def load_document():
+    label = ":blue[Upload a PDF document here]"
     uploaded_files = st.file_uploader(label, type=['pdf'], accept_multiple_files=False, key=None, help=None,
                                       on_change=None,
                                       args=None, kwargs=None, disabled=False, label_visibility="visible")
+    pdf_text_content = ""
     if uploaded_files:
         logging.info(f"Document {uploaded_files.name} successfully loaded")
         vdb = PDFVectorDBLoader(uploaded_files)
         pdf_text_content = vdb.get_pdf_content()
 
+    return pdf_text_content
+
+
+def generate_questions(pdf_text_content: str, topic: str, number: str):
+    question_json = ""
+    if pdf_text_content:
+        query = f"Given the following content about {topic}: {pdf_text_content}, generate a list of {number} with answers"
+        messages = [
+            SystemMessage(content=get_qa_system_prompt()),
+            HumanMessage(content=query)
+        ]
+        question_json = llm.invoke(messages).content.strip()
+    return question_json
 
 
 if __name__ == "__main__":
-    main()
+    pdf_text_content = load_document()
+    if pdf_text_content:
+        topic = st.text_input(":blue[Enter a topic:]", placeholder="eg AI")
+        number = st.text_input(":blue[Enter the number of question per test:]", placeholder="eg 5")
+        if topic and number:
+            question_json = generate_questions(pdf_text_content, topic, str(number))
+            logging.info(f"Generated question: {question_json}")
+
+            st.write(question_json)
