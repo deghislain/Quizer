c5c44a6e470ffef70c52c451769084b718bfc45e
deghislain <deghislain@gmail.com>
2025-03-31 10:27:22 -0300

    Code refactoring: moved the load document function to document helper module

--- document_helper.py
+++ document_helper.py
@@ -1,7 +1,28 @@
 from PyPDF2 import PdfReader
 import logging
+import streamlit as st
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def load_document():
+    """
+    Function to upload a PDF document and extract its text content.
+    :return: Text content of the uploaded PDF or an empty string if no file is uploaded.
+    """
+    label = ":blue[Upload a PDF document here]"
+    uploaded_files = st.file_uploader(label, type=['pdf'], accept_multiple_files=False)
+    pdf_text_content = ""
+    if uploaded_files:
+        try:
+            logging.info(f"Document {uploaded_files.name} successfully loaded")
+            pvdb = PDFVectorDBLoader(uploaded_files)
+            pdf_text_content = pvdb.get_pdf_content()
+        except Exception as e:
+            logging.error(f"Error processing PDF: {e}")
+            pdf_text_content = ""
+
+    return pdf_text_content
 
 
 class PDFVectorDBLoader:
--- quizzer.py
+++ quizzer.py
@@ -1,28 +1,14 @@
 import streamlit as st
-from document_helper import PDFVectorDBLoader
+from document_helper import load_document
 from langchain_openai import ChatOpenAI
 from qa_prompt import get_prompt
 from langchain.schema import HumanMessage
-from q_a_helper import create_test
+from qa_helper import create_test
 import logging
 import time
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 llm = ChatOpenAI(model="granite3-dense:latest", temperature=0, base_url="http://localhost:11434/v1", api_key="ollama")
-
-
-def load_document():
-    label = ":blue[Upload a PDF document here]"
-    uploaded_files = st.file_uploader(label, type=['pdf'], accept_multiple_files=False, key=None, help=None,
-                                      on_change=None,
-                                      args=None, kwargs=None, disabled=False, label_visibility="visible")
-    pdf_text_content = ""
-    if uploaded_files:
-        logging.info(f"Document {uploaded_files.name} successfully loaded")
-        vdb = PDFVectorDBLoader(uploaded_files)
-        pdf_text_content = vdb.get_pdf_content()
-
-    return pdf_text_content
 
 
 def generate_questions(pdf_text_content: str, topic: str, number: str):
@@ -44,9 +30,11 @@ if __name__ == "__main__":
     if pdf_text_content:
         topic = st.text_input(":blue[Enter a topic:]", placeholder="eg AI")
         number = st.text_input(":blue[Enter the number of question per test:]", placeholder="eg 5")
-        if topic and number:
+        btn_submit = st.button("Submit")
+        if btn_submit and topic and number:
             question_json = generate_questions(pdf_text_content, topic, str(number))
             logging.info(f"Generated question: {question_json}")
             test = create_test(question_json)
             logging.info("Test successfully created")
             st.write(question_json)
+
